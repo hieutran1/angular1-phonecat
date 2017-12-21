15cd2ecf82524328736073d92954f55ec855841e
Hieu Tran <hieutran@kms-technology.com>
2017-12-21 13:57:21 +0700

    Track config, run phase

--- app/app.config.js
+++ app/app.config.js
@@ -4,6 +4,7 @@ angular.
   module('phonecatApp').
   config(['$locationProvider' ,'$routeProvider', '$analyticsProvider',
     function config($locationProvider, $routeProvider, $analyticsProvider) {
+      console.log('[config]: state - ' + document.readyState);
       $analyticsProvider.firstPageview(true); /* Records pages that don't use $state or $route */
       $analyticsProvider.withAutoBase(true);  /* Records full path */
       
@@ -18,4 +19,10 @@ angular.
         }).
         otherwise('/phones');
     }
-  ]);
+  ]).
+  run(function(){
+    console.log('[run]: state - ' + document.readyState);
+    $(document).ready(function(){
+      console.log('[run-window]: state - ' + document.readyState);
+    });
+  });
